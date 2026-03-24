96968ef6c5952c218b861366e4b6ae68bb967210
YASH PAZHIANUR <yrp6541@nyu.edu>
2026-03-24 12:24:55 -0400

    more precise distance conversion

--- unit_convert/distance_converter.py
+++ unit_convert/distance_converter.py
@@ -12,10 +12,10 @@ def convert_distance(x: float, unit_from: UnitDistance, unit_to: UnitDistance) -
         "centimeter": 100,
         "meter":      1,
         "kilometer":  0.001,
-        "inch":       39.37,
-        "feet":       3.281,
-        "yard":       1.094,
-        "mile":       1/1609,
+        "inch":       39.3700787, # 39.37,
+        "feet":       3.2808399, # 3.281,
+        "yard":       1.0936133, # 1.094,
+        "mile":       1/1609.344 # 1/1609,
     }
 
     if(unit_from.value not in meter_to_unit_ratios):
