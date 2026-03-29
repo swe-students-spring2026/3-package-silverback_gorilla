4ba044b79c7a252591ad5b20def80cec21b10a47
nuzhny25 <sashanuzhny@gmail.com>
2026-03-29 13:50:27 -0400

    slightly altered the project directory structure to fit the standart

---
+++ src/unit_convert/converter_types.py
@@ -0,0 +1,47 @@
+
+class InvalidUnitError(Exception):
+    pass
+
+from enum import Enum
+
+class UnitTemperature(Enum):
+    C = "celsius"
+    F = "fahrenheit"
+    K = "kelvin"
+
+class UnitDistance(Enum):
+    FM = "femtometer"
+    PM = "picometer"
+    NM = "nanometer"
+    UM = "micrometer"
+    MM = "millimeter"
+    CM = "centimeter"
+    M  = "meter"
+    KM = "kilometer"
+    IN = "inch"
+    FT = "feet"
+    YD = "yard"
+    MI = "mile"
+
+
+class UnitData(Enum):
+    # Bytes
+    B = "byte"
+    KB = "kilobyte"
+    MB = "megabyte"
+    GB = "gigabyte"
+    TB = "terabyte"
+    PB = "petabyte"
+    EB = "exabyte"
+
+    # Bits
+    b = "bit"
+    Kb = "kilobit"
+    Mb = "megabit"
+    Gb = "gigabit"
+    Tb = "terabit"
+
+class UnitMass(Enum):
+    G = "gram"
+    P = "pound"
+    K = "kilogram"
---
+++ src/unit_convert/mass_converter.py
@@ -0,0 +1,44 @@
+from unit_convert.converter_types import InvalidUnitError, UnitMass
+import numbers
+
+NDIGITS = 8
+
+def convert_mass(x: float, unit_from: UnitMass, unit_to: UnitMass) -> float:
+
+    if not isinstance(x, numbers.Number):
+        raise TypeError("`x` is not a valid number")
+
+    if unit_from == unit_to: return x
+
+    #Convert to grams first
+    match unit_from:
+        case UnitMass.G:
+            pass
+        case UnitMass.P:
+            x = pounds_to_grams(x)
+        case UnitMass.K:
+            x = kilograms_to_grams(x)
+        case _:
+            raise InvalidUnitError("`unit_from` is not an accepted mass unit")
+
+    match unit_to:
+        case UnitMass.G:
+            return x
+        case UnitMass.P:
+            return grams_to_pounds(x)
+        case UnitMass.K:
+            return grams_to_kilograms(x)
+        case _:
+            raise InvalidUnitError("`unit_to is not an accepted mass unit")
+
+def pounds_to_grams(x: float) -> float:
+    return round(x * 453.59237, NDIGITS)
+
+def kilograms_to_grams(x: float) -> float:
+    return round(x * 1000, NDIGITS)
+
+def grams_to_pounds(x: float) -> float:
+    return round(x / 453.59237, NDIGITS)
+
+def grams_to_kilograms(x: float) -> float:
+    return round(x / 1000, NDIGITS)
