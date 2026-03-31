cf9f299607935a08ec23c7acf42ccf5806f830d2
CalvinPun <cp4295@nyu.edu>
2026-03-31 01:19:48 -0400

    readme mod

--- example.py
+++ example.py
@@ -1,17 +1,4 @@
-# Option 1:
-# from unit_convert import *
-
-# Option 2: explicit import
-from unit_convert import (
-    convert_data,
-    convert_distance,
-    convert_mass,
-    convert_temperature,
-    UnitData,
-    UnitDistance,
-    UnitMass,
-    UnitTemperature,
-)
+from unit_convert import *
 
 print("Data:", convert_data(1024, UnitData.KB, UnitData.MB))
 print("Distance:", convert_distance(1, UnitDistance.MI, UnitDistance.KM))
